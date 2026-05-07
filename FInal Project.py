ec4fda98e6ebe61c45cdb3689f27090e1e382e2b
nbelete1-alt <nbelete1@terpmail.umd.edu>
2026-05-07 13:39:26 -0400

    added file

---
+++ FInal Project.py
@@ -0,0 +1,9 @@
+#Code for the 2 players
+# Define exactly two players
+
+import numpy as np          
+import pandas as pd        
+import matplotlib.pyplot as plt
+
+players = ["Player 1", "Player 2"]
+scores = {players[0]: 0, players[1]: 0}
